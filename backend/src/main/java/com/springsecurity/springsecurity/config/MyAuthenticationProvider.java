35707e97c4e2235bc8fc0821d95c2429ce96ca8f
Aayush Ghising <aayushghising321@gmail.com>
2025-07-25 00:13:18 +0545

    add custome authentication provider

---
+++ backend/src/main/java/com/springsecurity/springsecurity/config/MyAuthenticationProvider.java
@@ -0,0 +1,54 @@
+package com.springsecurity.springsecurity.config;
+
+import com.springsecurity.springsecurity.model.Customer;
+import com.springsecurity.springsecurity.repo.CustomerRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class MyAuthenticationProvider implements AuthenticationProvider{
+
+    @Autowired
+    private CustomerRepo customerRepo;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        String username = authentication.getName();
+        String password = authentication.getCredentials().toString();
+
+        List<Customer> customers = customerRepo.findByEmail(username);
+        if (customers != null && customers.size() > 0) {
+            Customer customer = customers.get(0);
+            if (passwordEncoder.matches(password, customer.getPassword())){
+                List<GrantedAuthority> authorities = new ArrayList<>();
+                authorities.add(new SimpleGrantedAuthority(customer.getRole()));
+                return new UsernamePasswordAuthenticationToken(
+                        customer.getEmail(),
+                        customer.getPassword(),
+                        authorities // Assuming Customer has a method to get authorities
+                );
+            } else {
+                throw new AuthenticationException("Invalid password") {};
+            }
+        }
+        throw new AuthenticationException("User not found with username: " + username) {};
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return (UsernamePasswordAuthenticationToken.class.isAssignableFrom(authentication));
+    }
+}
